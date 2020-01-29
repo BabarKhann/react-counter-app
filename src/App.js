ac82b1e192271a5e390b9d9538059910b4bf417c
Babar Khan <babarkhan138@gmail.com>
2020-01-30 00:21:25 +0500

    create increment function, and set it into onClick event of + button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  inc = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>Counter</h2>
-        <p>2</p>
-        <button>Inc +</button>
+        <p>{this.state.counter}</p>
+        <button onClick={this.inc}>Inc +</button>
         <button>Dec -</button>
       </div>
     );
